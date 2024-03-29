881387d285912a04791002e8ace426de96846433
D-YF <daiyf1028@gmail.com>
2024-03-29 17:15:50 -0500

    searchbar with styles

--- src/components/Searchbar/searchbar.js
+++ src/components/Searchbar/searchbar.js
@@ -1,13 +1,36 @@
 import React from "react";
-import "./searchbar.css";
+import { useState, useEffect } from "react";
+import "./searchbar.scss";
+//event handler type
+const SearchBar = ({submitSearch}) => {
 
-const Searchbar = () => {
+    const [searchText, setSearchText] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        submitSearch({searchText});
+    };
+
+    useEffect(() => {
+        submitSearch({searchText});
+    }, [searchText, submitSearch]);
+
     return (
-        <div className="searchbar">
-            <input type="text" placeholder="Search..." />
-            <button>Search</button>
+        <div className="SearchBar">
+            <form onSubmit={(event) => handleSubmit(event)} className="SearchForm" action="./">
+                <span>
+                    <i className="fa fa-search" aria-hidden="true"></i>
+                </span>
+                <input 
+                    className="InputBox" 
+                    onChange={(event => setSearchText(event.target.value))}
+                    placeholder="Seach for anything" 
+                    type="search"
+                    value={searchText}
+                />
+            </form>
         </div>
     );
-};
+}
 
-export default Searchbar;
+export default SearchBar;
