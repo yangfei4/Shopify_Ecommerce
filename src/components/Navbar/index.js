44ca4c5c3b6035529f3f8289fa218581ed78f8d8
D-YF <daiyf1028@gmail.com>
2024-03-29 16:17:47 -0500

    initiated react project

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,4 @@
+import Navbar from "./navbar";
+import { memo } from "react";
+
+export default memo(Navbar);
---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./navbar.css";
+
+const Navbar = () => {
+    return (
+        <header className="navbar">
+            <nav className="navbar__items">
+                <div className="navbar__item">
+                    {/* icon from font-awsome */}
+                    <a href="/">
+                        Cart
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
